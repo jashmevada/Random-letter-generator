3fc35aedd4e0a53ddef71968b9f5f234d016776d
Jash Mevada <98532059+jashmevada@users.noreply.github.com>
2022-09-11 10:27:32 +0530

    Update random_letter_generator.py
    Update some comments.

--- random_letter_generator.py
+++ random_letter_generator.py
@@ -7,8 +7,8 @@ from numpy import random
 # Main fuction 
 def pw_gen(string: str,  length: int, n: int = 1): 
 	# 're_res' re -> rearrange and res -> result
-	# 'pw_raw' -> passworld list as raw data  
-	# 'pw' -> Passworld list
+	# 'pw_raw' -> password list as raw data  
+	# 'pw' -> password list
 
 	s = [] # for letters 
 	pw_raw = [] # to store all poassable valves 
@@ -19,13 +19,13 @@ def pw_gen(string: str,  length: int, n: int = 1):
 	# Main loop 
 	for i in tqdm(range(n)):
 		random.shuffle(s) # random the letters 
-		re = "".join(s[0:length]) # all random letter as per given length 
+		re = "".join(s[0:length]) # make random letter from list 's' of input length
 		pw = pw_raw.append(re) # save all at valve in 'pw' array 	
 	
 	end = time()
 	print("Time taken: ", (end - start))
-	res = list(OrderedDict.fromkeys(pw_raw)) # sort all thing like small elements 
-	re_res = "\n".join(res) # and make final array called 're_res' re -> rearrange and res -> result 
+	res = list(OrderedDict.fromkeys(pw_raw)) # Remove same letter from 'pw'. 
+	re_res = "\n".join(res) # final list with random letters and removed all same letter and only content unique letters. 
 	
 	# Print number of elements in all arrays 
 	print("re : " ,len(re),'\n',"pw_raw: ", len(pw_raw), '\n', "res : " ,len(res), '\n',"re_res : " ,len(re_res))
